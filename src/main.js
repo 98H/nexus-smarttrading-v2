2f63055573df585e8b2ddc8654945c6cbf993d0e
Hossein Mohammadi <mohammadi@98h.dev>
2026-09-20 10:36:06 +0000

    fix(main): clean trailing guard syntax

--- src/main.js
+++ src/main.js
@@ -648,20 +648,3 @@ export {
 };
 
 export default mountApp;
-
-// Browser Auto-Mount Bootstrap Guard
-
-// Browser Auto-Mount Bootstrap Guard
-if (typeof document !== 'undefined') {
-  const mountTarget = document.getElementById('app') || document.body;
-  if (mountTarget && !mountTarget.__nexus_mounted) {
-    mountTarget.__nexus_mounted = true;
-    if (typeof mountApp === 'function') {
-      mountApp(mountTarget);
-    } else if (typeof mount === 'function') {
-      mount(mountTarget);
-    } else if (typeof init === 'function') {
-      init(mountTarget);
-    }
-  }
-}
